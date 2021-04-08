978847ab518af984009814b23da9add6874b4f3d
Andrew Chidi <chidi.andrew@gmail.com>
2021-04-08 22:34:06 +0100

    Create hamburger.js
    hamburger menu

---
+++ js/hamburger.js
@@ -0,0 +1,6 @@
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav-links").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
